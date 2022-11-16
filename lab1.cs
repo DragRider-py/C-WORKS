64a4df054f75ea863cc6d79c612334ec5e6063b6
DragRider-py <71897558+DragRider-py@users.noreply.github.com>
2022-11-17 02:11:39 +0300

    Add files via upload

---
+++ lab1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            string strx = x.ToString();
+            string stry = y.ToString();
+
+            for (int i = 0; i < stry.Length; i++)
+            {
+                for (int j = 0; j < strx.Length; j++)
+                {
+                    if (stry[i] == strx[j]) { Console.WriteLine(strx[j]); }
+                }
+
+            }
+        }
+    }
+}
