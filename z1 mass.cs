fb6f6bb1e270921377c01a380024d7f21ae9d118
DragRider-py <71897558+DragRider-py@users.noreply.github.com>
2022-11-26 20:17:12 +0300

    Add files via upload

---
+++ z1 mass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            foreach (var letter in words)
+                Console.WriteLine(letter);
+        }
+    }
+}
